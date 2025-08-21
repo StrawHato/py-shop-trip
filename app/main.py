ec17bb28c61c3ba5a0b4c3cd2222a1cba923d839
Maksym Pakula <gargantadoto@gmail.com>
2025-08-21 20:24:32 +0300

    fixed some mistakes

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ shops_list = [
 
 def shop_trip() -> None:
     for customer in customers_list:
-        print(f"{customer["name"]} has {customer["money"]} dollars")
+        print(f"{customer.name} has {customer.money} dollars")
         trip_costs = {
             shop: distance_cost(customer, shop) + shop.products_cost(customer)
             for shop in shops_list
@@ -32,14 +32,14 @@ def shop_trip() -> None:
         cheapest_shop = min(trip_costs, key=trip_costs.get)
         min_cost = trip_costs[cheapest_shop]
 
-        if min_cost < customer["money"]:
-            print(f"{customer["name"]} rides to {cheapest_shop.name}\n")
+        if min_cost < customer.money:
+            print(f"{customer.name} rides to {cheapest_shop.name}\n")
             date = datetime(2021, 1, 4, 12, 33, 41)
             print(f"Date: {date.strftime("%d/%m/%Y %H:%M:%S")}")
-            print(f"Thanks, {customer["name"]}, for your purchase!")
+            print(f"Thanks, {customer.name}, for your purchase!")
             print("You have bought:")
             for key, value in customer.product_cart.items():
-                cost = (cheapest_shop.products[key] * value)
+                cost = cheapest_shop.products[key] * value
                 if str(cost).count(".0") > 0:
                     cost = int(cost)
                 print(f"{value} {key}s for {cost} dollars")
@@ -48,13 +48,13 @@ def shop_trip() -> None:
                 f"{cheapest_shop.products_cost(customer)} dollars"
             )
             print("See you again!\n")
-            print(f"{customer["name"]} rides home")
+            print(f"{customer.name} rides home")
             print(
-                f"{customer["name"]} now has "
-                f"{customer["money"] - min_cost} dollars\n"
+                f"{customer.name} now has "
+                f"{customer.money - min_cost} dollars\n"
             )
         else:
             print(
-                f"{customer["name"]} doesn't have "
+                f"{customer.name} doesn't have "
                 f"enough money to make a purchase in any shop"
             )
